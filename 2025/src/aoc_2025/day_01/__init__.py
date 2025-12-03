e9d5b7162b55621a6ba538e86db0a19ddd068f7c
Tom Brand <tom025@me.com>
2025-12-03 22:48:53 +0000

    2025: Day one part one solution

---
+++ 2025/src/aoc_2025/day_01/__init__.py
@@ -0,0 +1,40 @@
+import collections
+import typing
+
+Direction = typing.Literal['L', 'R']
+Rotation = tuple[Direction, int]
+
+
+def parse_rotation(r: str) -> Rotation:
+    return typing.cast(Direction, r[0]), int(r[1:])
+
+
+def move_dial(pos: int, rotation: Rotation) -> int:
+    direction, clicks = rotation
+    return (pos + clicks if direction == 'R' else pos - clicks) % 100
+
+
+def apply_rotations(start: int, rotations: collections.abc.Iterator[Rotation]) -> collections.abc.Generator[int, None, None]:
+    pos = start
+    for r in rotations:
+        pos = move_dial(pos, r)
+        yield pos
+
+
+def count_dial_at_zero(dial_positions: collections.abc.Iterator[int]) -> int:
+    return sum(1 for p in dial_positions if p == 0)
+
+
+def parse_rotations(text: typing.TextIO) -> collections.abc.Generator[Rotation, None, None]:
+    for line in text:
+        yield parse_rotation(line.strip())
+
+
+def solve(text: typing.TextIO) -> int:
+    return count_dial_at_zero(apply_rotations(50, parse_rotations(text)))
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        result = solve(f)
+
+    print(f"password is: {result!r}")
--- 2025/tests/test_day01.py
+++ 2025/tests/test_day01.py
@@ -1,2 +1,90 @@
-def test_day01():
-    assert True == False
+import collections
+import io
+import textwrap
+import typing
+
+import pytest
+
+from aoc_2025.day_01 import Rotation, parse_rotation, move_dial, apply_rotations, count_dial_at_zero, parse_rotations, \
+    solve
+
+
+@pytest.fixture
+def example_input() -> collections.abc.Generator[typing.TextIO, None, None]:
+    yield io.StringIO(textwrap.dedent("""\
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+    """))
+
+@pytest.fixture
+def example_rotations():
+    yield [
+        ('L', 68),
+        ('L', 30),
+        ('R', 48),
+        ('L', 5),
+        ('R', 60),
+        ('L', 55),
+        ('L', 1),
+        ('L', 99),
+        ('R', 14),
+        ('L', 82)
+    ]
+
+def test_day01_example(example_input: typing.TextIO) -> None:
+    assert solve(example_input) == 3
+
+def test_parse_input(example_input: typing.TextIO, example_rotations) -> None:
+    assert list(parse_rotations(example_input)) == example_rotations
+
+@pytest.mark.parametrize(
+    "rotation_str,expected",
+    [
+        ('L68', ('L', 68)),
+        ('R60', ('R', 60)),
+        ('R0', ('R', 0)),
+        ('L0', ('L', 0))
+    ],
+)
+def test_parse_rotation(rotation_str, expected):
+    assert parse_rotation(rotation_str) == expected
+
+@pytest.mark.parametrize(
+    "current_position,rotation,expected",
+    [
+        (50, ('L', 68), 82),
+        (82, ('L', 30), 52),
+        (52, ('R', 48), 0)
+    ]
+)
+def test_move_dial(current_position: int, rotation: Rotation, expected: int):
+    assert move_dial(current_position, rotation) == expected
+
+
+def test_apply_rotations(example_rotations: list[Rotation]):
+    dial_positions: list[int] = list(apply_rotations(50, iter(example_rotations)))
+    assert dial_positions == [
+        82,
+        52,
+        0,
+        95,
+        55,
+        0,
+        99,
+        0,
+        14,
+        32
+    ]
+
+
+def test_count_dial_at_zero(example_rotations: list[Rotation]) -> None:
+    assert count_dial_at_zero(apply_rotations(50, iter(example_rotations))) == 3
+
