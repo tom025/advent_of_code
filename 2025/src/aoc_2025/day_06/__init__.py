3e6489da905b51e8d35009848cd29a66643b4d71
Tom Brand <tom025@me.com>
2025-12-07 10:59:05 +0000

    2025: Day 06, part 2 solution
    Had a lot of trouble as my editor (Intellij) automatically trimmed the ends of line of the input file

--- 2025/src/aoc_2025/day_06/__init__.py
+++ 2025/src/aoc_2025/day_06/__init__.py
@@ -41,14 +41,80 @@ def solve_problem(p: Problem) -> int:
     return result
 
 
+def int_or_zero(s: str) -> int:
+    try:
+        return int(s)
+    except ValueError:
+        return 0
+
+
+def find_ops_and_prob_ranges(operations_line: str) -> list[tuple[Operator, int, int]]:
+    result = []
+    p_start = 0
+    op = None
+    for i in range(len(operations_line)):
+        if i == len(operations_line) - 1:
+            result.append((op, p_start, i))
+        c = operations_line[i]
+        if c in {'+', '*'}:
+            if i > 0:
+                result.append((op, p_start, i - 1))
+            op = parse_value(c)
+            p_start = i
+    return result
+
+
+def parse_caphalopod_problems(textio: typing.TextIO) -> list[tuple[Operator, list[list[int]]]]:
+    lines = textio.readlines()
+    num_lines = lines[:len(lines) - 1]
+    operations = lines[-1]
+
+    op_and_prob_ranges = find_ops_and_prob_ranges(operations)
+    return list(
+        (
+            op,
+            list(list(int_or_zero(c) for c in l[start:stop]) for l in num_lines)
+        )
+        for op, start, stop in op_and_prob_ranges
+    )
+
+
+def matrix_col(matrix: list[list[int]], i: int) -> list[int]:
+    return list(matrix[y][i] for y in range(len(matrix)) if matrix[y][i] != 0)
+
+
+def digits_to_number(digits: list[int]) -> int:
+    return sum(
+        d * (10 ** (len(digits) - i - 1)) for i, d in enumerate(digits)
+    )
+
+
+def solve_caphalopod_problem(problem: tuple[Operator, list[list[int]]]) -> int:
+    op, matrix = problem
+    m_width = len(matrix[0])
+
+    *_, result = itertools.accumulate(
+        (
+            digits_to_number(matrix_col(matrix, x))
+            for x in reversed(range(m_width))
+        ),
+        op
+    )
+
+    return result
+
 def solve(textio: typing.TextIO) -> tuple[int, int]:
     matrix = parse_matrix(textio)
     width = len(matrix[0])
     s1_ = sum(solve_problem(get_problem(matrix, p)) for p in range(width))
-    return s1_, 0
+
+    textio.seek(0)
+    s2_ = sum(solve_caphalopod_problem(p) for p in parse_caphalopod_problems(textio))
+    return s1_, s2_
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        s1, _ = solve(f)
+        s1, s2 = solve(f)
 
     print(f"s1: {s1!r}")
+    print(f"s2: {s2!r}")
--- 2025/tests/test_day06.py
+++ 2025/tests/test_day06.py
@@ -4,7 +4,8 @@ import textwrap
 
 import pytest
 
-from aoc_2025.day_06 import solve, parse_matrix, get_problem, solve_problem, Problem
+from aoc_2025.day_06 import solve, parse_matrix, get_problem, solve_problem, parse_caphalopod_problems, \
+    solve_caphalopod_problem
 
 
 @pytest.fixture
@@ -27,13 +28,119 @@ def example_matrix():
 
 
 def test_day06_example(example_input) -> None:
-    s1, _ = solve(example_input)
+    s1, s2 = solve(example_input)
     assert s1 == 4277556
+    assert s2 == 3263827
 
 
 def test_parse_matrix(example_input, example_matrix):
     assert parse_matrix(example_input) == example_matrix
 
+
+@pytest.mark.parametrize(
+    "problem_index, expected_problem",
+    [
+        (
+                0,
+                (
+                        operator.mul,
+                        [
+                            [1, 2, 3],
+                            [0, 4, 5],
+                            [0, 0, 6]
+                        ]
+                )
+        ),
+        (
+                1,
+                (
+                        operator.add,
+                        [
+                            [3, 2, 8],
+                            [6, 4, 0],
+                            [9, 8, 0]
+                        ]
+                )
+        ),
+        (
+                2,
+                (
+                        operator.mul,
+                        [
+                            [0, 5, 1],
+                            [3, 8, 7],
+                            [2, 1, 5]
+                        ]
+                )
+        ),
+        (
+                3,
+                (
+                        operator.add,
+                        [
+                            [6, 4, 0],
+                            [2, 3, 0],
+                            [3, 1, 4]
+                        ]
+                )
+        )
+    ]
+)
+def test_parse_caphalopod_problems(example_input, problem_index, expected_problem):
+    assert parse_caphalopod_problems(example_input)[problem_index] == expected_problem
+
+
+@pytest.mark.parametrize(
+    "problem, expected_solution",
+    [
+        (
+                (
+                        operator.mul,
+                        [
+                            [1, 2, 3],
+                            [0, 4, 5],
+                            [0, 0, 6]
+                        ]
+                ),
+                8544
+        ),
+        (
+                (
+                        operator.add,
+                        [
+                            [3, 2, 8],
+                            [6, 4, 0],
+                            [9, 8, 0]
+                        ]
+                ),
+                625
+        ),
+        (
+                (
+                        operator.mul,
+                        [
+                            [0, 5, 1],
+                            [3, 8, 7],
+                            [2, 1, 5]
+                        ]
+                ),
+                3253600
+        ),
+        (
+                (
+                        operator.add,
+                        [
+                            [6, 4, 0],
+                            [2, 3, 0],
+                            [3, 1, 4]
+                        ]
+                ),
+                1058
+        )
+    ]
+)
+def test_solve_caphalopod_problem(problem, expected_solution):
+    assert solve_caphalopod_problem(problem) == expected_solution
 
 @pytest.mark.parametrize(
     "col_index, expected",
@@ -59,4 +166,3 @@ def test_get_problem(example_matrix, col_index, expected):
 )
 def test_solve_problem(example_matrix, col_index, expected):
     assert solve_problem(get_problem(example_matrix, col_index)) == expected
-
